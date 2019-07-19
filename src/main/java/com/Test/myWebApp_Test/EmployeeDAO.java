fb5c3c2a284ecd5b3941e2686b98b6b7d4642474
papun12345 <50276904+papun12345@users.noreply.github.com>
2019-07-19 15:24:34 +0530

    Final Commit

--- src/main/java/com/Test/myWebApp_Test/EmployeeDAO.java
+++ src/main/java/com/Test/myWebApp_Test/EmployeeDAO.java
@@ -12,8 +12,9 @@ public class EmployeeDAO
     static
     {
         list.getEmployeeList().add(new Employee(1, "Subha", "Satapathy", "papunpikun@gmail.com"));
-        list.getEmployeeList().add(new Employee(2, "Alex", "Kolenchiskey", "abc@gmail.com"));
-        list.getEmployeeList().add(new Employee(3, "David", "Kameron", "titanic@gmail.com"));
+        list.getEmployeeList().add(new Employee(2, "Samlee", "Mohanty", "samleemohanty97@gmail.com"));
+        list.getEmployeeList().add(new Employee(3, "Sudipa", "Behera", "sudipabehera24@gmail.com"));
+        list.getEmployeeList().add(new Employee(4, "Hemant", "Tiwari", "amanhemant01@gmail.com"));
     }
      
     public Employees getAllEmployees()
