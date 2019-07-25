db2c68140b32336be51e87f975edfefd0eea9f63
papun12345 <50276904+papun12345@users.noreply.github.com>
2019-07-25 22:04:08 +0530

    Update AppController.java

--- src/main/java/com/Test/myWebApp_Test/AppController.java
+++ src/main/java/com/Test/myWebApp_Test/AppController.java
@@ -3,12 +3,14 @@ package com.Test.myWebApp_Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.Test.myWebApp_Test.service.LoginService;
 
+@CrossOrigin(origins="http://localhost:3000")
 @Controller
 public class AppController {
 	@Autowired
