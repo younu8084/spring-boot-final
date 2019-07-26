ef2b29d1e852f0a8c23b95dadf6e0c680da23b2f
papun12345 <50276904+papun12345@users.noreply.github.com>
2019-07-26 12:27:08 +0530

    Update EmployeeController.java

--- src/main/java/com/Test/myWebApp_Test/EmployeeController.java
+++ src/main/java/com/Test/myWebApp_Test/EmployeeController.java
@@ -4,6 +4,7 @@ import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
  
 
- 
+@CrossOrigin(origins="http://18.224.182.74:7000/")
 @RestController
 @RequestMapping(path="/employees")
 public class EmployeeController
