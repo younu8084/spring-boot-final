3d348392300235c010ea1471c5b4d5770a5e8e44
Dharanidharkrish <55631557+Dharanidharkrish@users.noreply.github.com>
2019-11-13 15:07:24 +0530

    Update gvar.groovy

--- vars/gvar.groovy
+++ vars/gvar.groovy
@@ -1,3 +1,4 @@
 import groovy.transform.Field
 
 @Field String url = 'http://3.19.65.223:8990/'
+@Field String nexusRepoUrl = 'http://ec2-18-224-155-110.us-east-2.compute.amazonaws.com:8081/nexus/content/repositories/devopstraining/Codekillers/myWebApp_Test-${BUILD_NUMBER}.war'
