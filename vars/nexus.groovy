ffd2837020f76052daa6234a36c3f12637109231
Dharanidharkrish <55631557+Dharanidharkrish@users.noreply.github.com>
2019-11-13 15:16:02 +0530

    Update nexus.groovy

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -1,7 +1,7 @@
 def call(String msg = 'NEXUS'){
   echo "${msg}"
   withCredentials([usernamePassword(credentialsId: 'nexus-credentials', passwordVariable: 'pass', usernameVariable: 'userId')]) {
-         sh   'curl -F file=@target/myWebApp_Test-${BUILD_NUMBER}.war -u $userId:$pass gvar.nexusRepoUrl/myWebApp_Test-${BUILD_NUMBER}.war'
+         sh   'curl -F file=@target/myWebApp_Test-${BUILD_NUMBER}.war -u $userId:$pass gvar.nexusRepoUrl'
 			  } 
 			  
        			}
